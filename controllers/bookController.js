360e30efacf0d672eba51fa1c1930fe8f524b7cc
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-22 01:17:19 +0100

    set up controller to find all books, find by id, update and remove

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,49 @@
 const db = require("../models");
+
+module.exports = {
+    findAll: async (req, res) => {
+        try {
+            const books = await db.Book.find(req.query);
+            return res.json(books);
+        } catch (err) {
+            return res.status(422).json(err);
+        }
+    },
+
+    findById: async (req, res) => {
+        try {
+            const books = await db.Book.findById(req.params.id);
+            return res.json(books);
+        } catch (err) {
+            return res.status(422).json(err);
+        }
+    },
+
+    create: async (req, res) => {
+        try {
+            const books = await db.Book.create(req.body);
+            return res.json(books);
+        } catch (err) {
+            return res.status(422).json(err);
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+            const books = await db.Book.findOneAndUpdate({ id: req.params.id }, req.body);
+            return res.json(books);
+        } catch (err) {
+            return res.status(422).json(err);
+        }
+    },
+
+    remove: async (req, res) => {
+        try {
+            const books = await db.Book.findById(req.params.id);
+            const deleteBook = await books.remove();
+            return res.json(deleteBook);
+        } catch (err) {
+            return res.status(422).json(err);
+        }
+    }
+};
