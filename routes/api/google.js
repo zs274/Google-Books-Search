aad4612264d05b2f95f805e9b5f6e166af37dee0
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-23 01:09:40 +0100

    copied files out, restarting app bc made an error messing with package json files

--- routes/api/google.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const googleController = require("../../controllers/googleController");
-
-router
-  .route("/")
-  .get(googleController.findAll);
-
-module.exports = router;
